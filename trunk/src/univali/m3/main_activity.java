6443df25a5c143133d71ce254aa01cb3e93f93c6
guilhermopazuch@gmail.com <guilhermopazuch@gmail.com@ce331436-e0c0-72a2-bde8-2a726b18e860>
2012-06-20 02:32:01 +0000

    Primeiro Commit

---
+++ trunk/src/univali/m3/main_activity.java
@@ -0,0 +1,13 @@
+package univali.m3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main_activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
