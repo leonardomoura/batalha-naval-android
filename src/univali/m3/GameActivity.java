078251a170b8a22af88aedaec405bd05f5a531f2
guilhermopazuch@gmail.com <guilhermopazuch@gmail.com@ce331436-e0c0-72a2-bde8-2a726b18e860>
2012-07-02 02:36:24 +0000

    nao sei que porra aconteceu, mas depois de mudar a thread para dentro do surface view ao inves da activity, a porra comecou a funcionar

--- src/univali/m3/GameActivity.java
+++ src/univali/m3/GameActivity.java
@@ -5,44 +5,14 @@ import android.graphics.Canvas;
 import android.os.Bundle;
 
 public class GameActivity extends Activity {
-	Boolean running = false;
-	Thread gamethread;
 	GameSurface gamesurface;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		gamesurface = new GameSurface(GameActivity.this);
+		gamesurface = new GameSurface(this);
 		setContentView(gamesurface);
-		
-		gamethread = new Thread(){
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				running=true;
-				super.run();
-				Canvas c=null;
-				while(running){
-					update();
-					try{
-						c = gamesurface.getHolder().lockCanvas();
-						synchronized (gamesurface.getHolder()) {
-							gamesurface.onDraw(c);
-						}
-					}finally{
-						if(c!=null){
-							gamesurface.getHolder().unlockCanvasAndPost(c);
-						}
-					}
-					
-				}
-			}
-		};
-		gamethread.run();
-	}
-	
-	protected void update(){
-		
 	}
 }
--- src/univali/m3/GameSurface.java
+++ src/univali/m3/GameSurface.java
@@ -3,19 +3,60 @@ package univali.m3;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Rect;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
-
+	Boolean running = false;
+	Thread gamethread;
+	
 	public GameSurface(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
+		getHolder().addCallback(this);
+		gamethread = new Thread(){
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				running=true;
+				super.run();
+				Canvas c=null;
+				while(running){
+					try{
+						c = getHolder().lockCanvas();
+						
+					}finally{
+						if(c!=null){
+							onDraw(c);
+							getHolder().unlockCanvasAndPost(c);
+						}
+						try {
+							sleep(20);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					
+				}
+			}
+		};
 	}
+	
 	@Override
 	protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.BLACK);
+		
+        canvas.drawColor(Color.WHITE);
 //        canvas.drawBitmap(bmp, 10, 10, null);
+        canvas.drawColor(Color.BLUE);
+        Paint p = new Paint();
+        p.setColor(Color.RED);
+        p.setAlpha(1);
+        p.setStyle(Style.FILL);
+        canvas.drawRect(new Rect(0, 0, 100, 100), p);
     }
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
@@ -26,11 +67,12 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
-		
+		gamethread.start();
 	}
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
+		running=false;
 		
 	}
 
