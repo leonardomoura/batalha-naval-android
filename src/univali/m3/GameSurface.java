e7cc43491c99c8517e8237363c45e5ebbb51c161
guilhermopazuch@gmail.com <guilhermopazuch@gmail.com@ce331436-e0c0-72a2-bde8-2a726b18e860>
2012-07-04 02:33:20 +0000

    mas que porra, agora que o touch funciona, nao consigo carregar imagens

--- src/univali/m3/GameSurface.java
+++ src/univali/m3/GameSurface.java
@@ -1,33 +1,57 @@
 package univali.m3;
 
+import java.util.Random;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.Align;
 import android.graphics.Paint.Style;
 import android.graphics.Rect;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
 
 public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
 	Boolean running = false;
 	Thread gamethread;
 	
-	public BaseShip[] myships = new BaseShip[3+2+3];
-	public BaseShip[] theirships = new BaseShip[3+2+3];
+	float rawY=0;
+	float rawX=0;
+	float mouseX=0;
+	float mouseY=0;
 	
+	//isso vai ser muito gambiarristico
+	int cliques;
+	
+	Context c;
+	
+	public BaseShip[] myships = new BaseShip[3];
+	public BaseShip[] theirships = new BaseShip[3];
+	
+	public Bitmap grid;
 	
 	public GameSurface(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub		
+		c = context;
+		cliques=0;
+		
+		Bitmap grid = BitmapFactory.decodeResource(getResources(), R.drawable.grid);
 		
 		for(int i=0; i<3; i++){
 			myships[i] = new BoatShip();
-			myships[i].x = i;
-			myships[i].y = 0;
+			myships[i].x = new Random().nextInt(6);
+			myships[i].y = new Random().nextInt(3);
 			theirships[i] = new BoatShip();
-			theirships[i].x = 0;
-			theirships[i].y = i;
+			theirships[i].x = new Random().nextInt(6);
+			theirships[i].y = new Random().nextInt(3);
 		}
 		
 		
@@ -67,37 +91,23 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
+		Paint p = new Paint();
+		// limpa a tela
+		p.setColor(Color.BLACK);
+		p.setStyle(Style.FILL_AND_STROKE);
+		canvas.drawColor(Color.BLACK);
+//		p = new Paint();
+//		p.setColor(Color.WHITE);
+//		p.setTextAlign(Align.LEFT);
+//		p.setStyle(Style.FILL_AND_STROKE);
+//		canvas.drawText("rawX "+rawX, 10, 10, p);
+//		canvas.drawText("rawY "+rawY, 10, 30, p);
+//		canvas.drawText("mouseX "+mouseX, 10, 50, p);
+//		canvas.drawText("mouseY "+mouseY, 10, 70, p);
 		
-        canvas.drawColor(Color.WHITE);
-//        canvas.drawBitmap(bmp, 10, 10, null);
-        canvas.drawColor(Color.BLUE);
-        Paint p = new Paint();
-        p.setColor(Color.WHITE);
-
-        for(int i=0; i<3; i++){
-        	canvas.drawText(myships[i].className, myships[i].x*canvas.getWidth()/6+10, 
-        			myships[i].y*canvas.getHeight()/6+canvas.getHeight()/2+20, p);
-        	canvas.drawText(theirships[i].className, theirships[i].x*canvas.getWidth()/6+10, 
-        			theirships[i].y*canvas.getHeight()/6+20, p);
-        	
-        }
-        
-        
-//        p.setAlpha(1);
-        p.setStyle(Style.FILL);       
-        p.setStyle(Style.STROKE);
-        canvas.drawLine(0, canvas.getHeight()/6, canvas.getWidth(), canvas.getHeight()/6, p);
-        canvas.drawLine(0, canvas.getHeight()/6*2, canvas.getWidth(), canvas.getHeight()/6*2, p);
-        p.setColor(Color.BLACK);
-        canvas.drawLine(0, canvas.getHeight()/2, canvas.getWidth(), canvas.getHeight()/2, p);
-        p.setColor(Color.WHITE);
-        canvas.drawLine(0, canvas.getHeight()/6*4, canvas.getWidth(), canvas.getHeight()/6*4, p);
-        canvas.drawLine(0, canvas.getHeight()/6*5, canvas.getWidth(), canvas.getHeight()/6*5, p);
-        canvas.drawLine(canvas.getWidth()/6, 0, canvas.getWidth()/6, canvas.getHeight(), p);
-        canvas.drawLine(canvas.getWidth()/6*2, 0, canvas.getWidth()/6*2, canvas.getHeight(), p);
-        canvas.drawLine(canvas.getWidth()/2, 0, canvas.getWidth()/2, canvas.getHeight(), p);
-        canvas.drawLine(canvas.getWidth()/6*4, 0, canvas.getWidth()/6*4, canvas.getHeight(), p);
-        canvas.drawLine(canvas.getWidth()/6*5, 0, canvas.getWidth()/6*5, canvas.getHeight(), p);
+		
+	    
+	    canvas.drawBitmap(grid, 0, 0, p);
     }
 	
 	@Override
@@ -116,6 +126,13 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
 		// TODO Auto-generated method stub
 		running=false;
 		
+	}
+	
+	public void didTouch(MotionEvent e){
+		this.rawX = e.getRawX();
+		this.rawY = e.getRawY();
+		this.mouseX = e.getX();
+		this.mouseY = e.getY();
 	}
 
 }
